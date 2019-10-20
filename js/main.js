cf334810e7c543dd3a9388f9a7544173da9d5b74
Georgi Popov <magadanski_uchen@yahoo.com>
2019-10-20 17:44:09 +0300

    Animate headings

--- js/main.js
+++ js/main.js
@@ -9,3 +9,17 @@ const introTitlePin = new ScrollMagic.Scene({ triggerElement: '.intro__title', t
 const titleSwipe = new ScrollMagic.Scene({ triggerElement: '.intro__title', triggerHook: 0, duration: 50 * vh })
 	.setTween(new TweenLite.fromTo('.intro__title', 1, { x: 0 }, { x: '100%' }))
 	.addTo(controller);
+
+const screens = document.querySelectorAll('.screen:not(.screen--intro)');
+
+screens.forEach(screen => {
+	const heading = screen.querySelector('.screen__title');
+	
+	new ScrollMagic.Scene({ triggerElement: screen, triggerHook: 0 })
+		.setPin(heading)
+		.addTo(controller);
+	
+	new ScrollMagic.Scene({ triggerElement: screen, triggerHook: 0.55, duration: 90 * vh })
+		.setTween(new TweenLite.fromTo(heading, 1, { x: '-100%', y: 0 }, { x: '100%', y: 0 }))
+		.addTo(controller);
+});
